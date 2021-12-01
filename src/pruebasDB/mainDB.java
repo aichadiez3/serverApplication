a6b4d20b47a3d078ac47a5430d9ef6e2fcf64e58
ALvaro-REdondo <ALVAROEIVISSA13@GMAIL.COM>
2021-12-01 17:23:26 +0100

    main para probar cosas

---
+++ src/pruebasDB/mainDB.java
@@ -0,0 +1,30 @@
+package pruebasDB;
+
+import SQLite.SQLiteMethods;
+
+import java.sql.SQLException;
+
+import SQLite.SQLiteManager;
+import pojos.*;
+
+public class mainDB {
+public static void main(String[] args) throws SQLException {
+		
+		SQLiteManager manager = new SQLiteManager();
+		boolean everything_ok = manager.Connect();
+		boolean tables_ok = manager.CreateTables();
+		System.out.println(manager.getSqlite_connection().getWarnings());
+		
+		System.out.println(everything_ok + " ");
+		
+		
+		SQLiteMethods methods = new SQLiteMethods();
+		User user = methods.Insert_new_user("name", "123", "aa@aa");
+		
+		System.out.println(methods.List_all_users());
+		
+		manager.Close_connection();
+		
+		
+	}
+}
