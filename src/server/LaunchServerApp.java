736f5e831c596a966cec4ec630e46fea38ba31bd
Aïcha <a.diez22@usp.ceu.es>
2021-12-05 10:20:47 +0100

    vuelta atrás

--- src/server/LaunchServerApp.java
+++ src/server/LaunchServerApp.java
@@ -2,10 +2,7 @@ package server;
 
 import java.io.IOException;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 import SQLite.SQLiteManager;
 import SQLite.SQLiteMethods;
@@ -22,11 +19,6 @@ import javafx.stage.StageStyle;
 public class LaunchServerApp extends Application {
 
 	private static Stage stage;
-    private SQLiteMethods methods;
-    private ServerSocket serverSocket;
-    private Socket socket;
-    
-	
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -36,102 +28,16 @@ public class LaunchServerApp extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		try {
-			
-			Parent root = FXMLLoader.load(getClass().getResource("ServerApplicationView.fxml"));
-			primaryStage.setTitle("Log in page");
-			Scene scene = new Scene(root);
-			scene.getStylesheets().addAll(getClass().getResource("dark-theme.css").toExternalForm());
-			primaryStage.initStyle(StageStyle.UNDECORATED);
-			primaryStage.setScene(scene);
-			stage = primaryStage;
-			primaryStage.show();
-			
-			/*
-			controller.Connect();
-			controller.CreateTables();
-			controller = null;
-			
-			// PREDETERMINED USEFUL INFORMATION FOR THE APPLICATION. NEED TO BE INSIDE THE DATABASE ONCE
-			
-			methods.Insert_new_insurance("Anthem");
-			methods.Insert_new_insurance("Centene");
-			methods.Insert_new_insurance("UnitedHealth");
-			methods.Insert_new_insurance("HCSC");
-			methods.Insert_new_insurance("DKV");
-			methods.Insert_new_insurance("Sanitas");
-			methods.Insert_new_insurance("Maphre");
-			methods.Insert_new_insurance("AXA");
-			methods.Insert_new_insurance("Asisa");
-			methods.Insert_new_insurance("Adeslas");
-			methods.Insert_new_insurance("Caser");
-			methods.Insert_new_insurance("Allianz");
-			methods.Insert_new_insurance("Aegon");
-			methods.Insert_new_insurance("Other");
-			
-			methods.Insert_new_doctor("Jose Luis García", "UnitedHealth");
-			methods.Insert_new_doctor("Carlos Ruíz", "Maphre");
-			methods.Insert_new_doctor("Marta Martínez", "Anthem");
-			methods.Insert_new_doctor("Laura Esteban", "Centene");
-			methods.Insert_new_doctor("Julia Medea", "HCSC");
-			methods.Insert_new_doctor("Maria José García", "DKV");
-			
-			*/
-			
-			// SERVER CREATION
-			
-			serverSocket = null;
 		
-			while(true){
-	            //This executes when we have a client
-				socket = null;
-				try {
-					serverSocket = new ServerSocket(9000);
-		            while (true) {
-		                //This executes when we have a patient
-		                socket = serverSocket.accept();
-		                new Thread(new ServerToDB(socket)).start();		
-		                
-		                // condition receive a message from a close instruction from client application (button x, log_out..)
-		                releaseResources(socket);
-		            }
-		        } catch (IOException e) {
-		        	Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, e);
-				}
-				finally {
-					releaseResourcesServer(socket, serverSocket);
-				}
-			}
+		Parent root = FXMLLoader.load(getClass().getResource("ServerApplicationView.fxml"));
+		primaryStage.setTitle("Log in page");
+		Scene scene = new Scene(root);
+		scene.getStylesheets().addAll(getClass().getResource("dark-theme.css").toExternalForm());
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setScene(scene);
+		stage = primaryStage;
+		primaryStage.show();
 			
-		} catch(IOException fatal_server_error) {
-			fatal_server_error.printStackTrace();
-			System.exit(0);
-		}
 	}
-	
-	
-	
-	
-	private static void releaseResources(Socket socket) {
-		try {
-            socket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
-        }
-	}
-	
-	public static void releaseResourcesServer(Socket socket, ServerSocket serverSocket) {
-		try {
-            socket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
-        }
-		
-		try {
-			serverSocket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
-        }
-	}
-	
+
 }
--- src/server/ServerController.java
+++ src/server/ServerController.java
@@ -1,10 +1,16 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import SQLite.SQLiteManager;
+import SQLite.SQLiteMethods;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
@@ -16,6 +22,7 @@ import javafx.stage.Stage;
 public class ServerController implements Initializable {
 
 	private SQLiteManager controller;
+    private SQLiteMethods methods;
 	
 	@FXML
     private Pane serverScene;
@@ -34,14 +41,67 @@ public class ServerController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1){
 		
 		controller = new SQLiteManager();
+		/*
+		controller.Connect();
+		controller.CreateTables();
+		controller = null;
+		
+		// PREDETERMINED USEFUL INFORMATION FOR THE APPLICATION. NEED TO BE INSIDE THE DATABASE ONCE
+		
+		methods.Insert_new_insurance("Anthem");
+		methods.Insert_new_insurance("Centene");
+		methods.Insert_new_insurance("UnitedHealth");
+		methods.Insert_new_insurance("HCSC");
+		methods.Insert_new_insurance("DKV");
+		methods.Insert_new_insurance("Sanitas");
+		methods.Insert_new_insurance("Maphre");
+		methods.Insert_new_insurance("AXA");
+		methods.Insert_new_insurance("Asisa");
+		methods.Insert_new_insurance("Adeslas");
+		methods.Insert_new_insurance("Caser");
+		methods.Insert_new_insurance("Allianz");
+		methods.Insert_new_insurance("Aegon");
+		methods.Insert_new_insurance("Other");
+		
+		methods.Insert_new_doctor("Jose Luis García", "UnitedHealth");
+		methods.Insert_new_doctor("Carlos Ruíz", "Maphre");
+		methods.Insert_new_doctor("Marta Martínez", "Anthem");
+		methods.Insert_new_doctor("Laura Esteban", "Centene");
+		methods.Insert_new_doctor("Julia Medea", "HCSC");
+		methods.Insert_new_doctor("Maria José García", "DKV");
+		
+		*/
 		
 		startButton.setOnMouseClicked((MouseEvent event) -> {
 			
 			// close the scene but the server still waiting for connections
 			main_menu_stage = (Stage) serverScene.getScene().getWindow();
 			main_menu_stage.setIconified(true);
-
 			
+			// SERVER CREATION
+			
+			ServerSocket serverSocket = null;
+		
+			while(true){
+	            //This executes when we have a client
+				Socket socket = null;
+				try {
+					serverSocket = new ServerSocket(9000);
+		            while (true) {
+		                //This executes when we have a patient
+		                socket = serverSocket.accept();
+		                new Thread(new ServerToDB(socket)).start();		
+		                
+		                // condition receive a message from a close instruction from client application (button x, log_out..)
+		                releaseResources(socket);
+		            }
+		        } catch (IOException e) {
+		        	Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, e);
+				}
+				finally {
+					releaseResourcesServer(socket, serverSocket);
+				}
+			}
 			
 		});
 		
@@ -50,10 +110,31 @@ public class ServerController implements Initializable {
 			//LaunchServerApp.releaseResourcesServer(LaunchServerApp.socket,LaunchServerApp.serverSocket); //--------> Esto no funchiona
 			System.exit(0);
 		});
-	}
+	
+}
     
 	
+	private static void releaseResources(Socket socket) {
+		try {
+	        socket.close();
+	    } catch (IOException ex) {
+	        Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
 	
+	public static void releaseResourcesServer(Socket socket, ServerSocket serverSocket) {
+		try {
+	        socket.close();
+	    } catch (IOException ex) {
+	        Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+		
+		try {
+			serverSocket.close();
+	    } catch (IOException ex) {
+	        Logger.getLogger(LaunchServerApp.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
 	
 	
 }
